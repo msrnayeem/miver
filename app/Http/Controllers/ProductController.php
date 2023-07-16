70d229b5509276393756542f45f61ffe9a83dc66
@msrnayeem <msrnayeem@gmail.com>
2023-07-16 16:02:53 +0600

    filter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,30 +10,28 @@ class ProductController extends Controller
     public function showAllProducts(Request $request)
     {
         $query = Product::query();
-        
+
         // Filter products based on price range
         $priceRange = $request->input('price-range');
         if ($priceRange) {
-                $query->whereBetween('price', [0, $priceRange]); 
+            $query->whereBetween('price', [0, $priceRange]);
         }
-    
+
         // Filter products based on category
         $category = $request->input('category');
-       
+
         if ($category) {
             $query->where('sub_sub_category_id', $category);
         }
-    
+
         // Include the user relationship
         $query->with('user');
-    
+
         // Paginate the filtered products
         $products = $query->paginate(10);
-    
+
         return view('pages.product.allProduct', compact('products'));
     }
-    
-
 
     public function singleProduct($id)
     {
